73bed5324048f12903c08afcfda84f6b4f71d527
Alejandro Baez <code@baez.nyc>
2016-06-05 19:14:39 -0400

    adding flatpak options

--- completions/flatpak.fish
+++ completions/flatpak.fish
@@ -1,1 +1,4 @@
 
+complete -c flatpak -s h -l help -d "Show help options and exit."
+complete -c flatpak -s v -l verbose -d "Print debug information during command processing."
+complete -c flatpak -l version -d "Print version information and exit."
