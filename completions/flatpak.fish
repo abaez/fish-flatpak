e61b6a9051386308ffa25e624c4b5e7a7db77aea
Alejandro Baez <code@baez.nyc>
2016-06-05 23:49:07 -0400

    adding install

--- completions/flatpak.fish
+++ completions/flatpak.fish
@@ -29,3 +29,17 @@ end
 complete -c flatpak -s h -l help -d "Show help options and exit."
 complete -c flatpak -s v -l verbose -d "Print debug information during command processing."
 complete -c flatpak -l version -d "Print version information and exit."
+
+
+# install
+complete -c flatpak -n "__fish_flatpak_command install" -d "Install an application or runtime"
+complete -c flatpak -n "__fish_flatpak_command install" -l bundle -d "Install from a bundle file instead of a configured remote."
+complete -c flatpak -n "__fish_flatpak_command install" -l user -d "Create a per-user installation."
+complete -c flatpak -n "__fish_flatpak_command install" -l system -d "Create a system-wide installation."
+complete -c flatpak -n "__fish_flatpak_command install" -l arch -d "The architecture to install for."
+complete -c flatpak -n "__fish_flatpak_command install" -l subpath -d "Install only a subpath of the ref."
+complete -c flatpak -n "__fish_flatpak_command install" -l no-deploy -d "Don't download the latest version and don't deploy it."
+complete -c flatpak -n "__fish_flatpak_command install" -l no-pull -d "Don't download the latest version but deploy it."
+complete -c flatpak -n "__fish_flatpak_command install" -l app -d "Only look for an app with the given name."
+complete -c flatpak -n "__fish_flatpak_command install" -l runtime -d "Only look for an runtime with the given name."
+
