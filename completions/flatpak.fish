023a3c3298b11d141118cadd49783e80b3f55414
Alejandro Baez <code@baez.nyc>
2016-06-09 17:00:47 -0400

    have now install,update, uninstall, and list

--- completions/flatpak.fish
+++ completions/flatpak.fish
@@ -38,11 +38,11 @@ complete -c flatpak -s h -l help -d "Show help options and exit."
 complete -c flatpak -s v -l verbose -d "Print debug information during command processing."
 complete -c flatpak -l version -d "Print version information and exit."
 
-# command sub commands
+# command sub commands: cmd [list of commands]
 function __fish_flatpak_samesub
   set -l cmd $argv[1]
 
-  for sub in $argv[2..]
+  for sub in $argv[2..-1]
     switch $sub
       case user
         complete -c flatpak -n "__fish_flatpak_command $argv" -l user -d "$argv a per-user installation."
@@ -60,14 +60,22 @@ function __fish_flatpak_samesub
         complete -c flatpak -n "__fish_flatpak_command $argv" -l app -d "Only look for an app with the given name."
       case runtime
         complete -c flatpak -n "__fish_flatpak_command $argv" -l runtime -d "Only look for an runtime with the given name."
+    end
   end
 end
 
 # install
 complete -c flatpak -n "__fish_flatpak_command install" -l bundle -d "Install from a bundle file instead of a configured remote."
-__fish_flatpak_samesub install
+__fish_flatpak_samesub install user system arch subpath no-deploy no-pull app runtime
 
 # update
 complete -c flatpak -n "__fish_flatpak_command update" -l commit -d "Update to this commit, instead of the tip of the branch."
-__fish_flatpak_samesub update
+__fish_flatpak_samesub update user system arch subpath no-doploy no-pull app runtime
 
+# uninstall
+complete -c flatpak -n "__fish_flatpak_command uninstall" -l keep-ref -d "Keep the ref for the application and the objects belonging to it in the local repository."
+__fish_flatpak_samesub uninstall user system arch app runtime
+
+# list
+complete -c flatpak -n "__fish_flatpak_command uninstall" -l show-details -d "Show arches and branches, in addition to the application names."
+__fish_flatpak_samesub list user system app runtime
