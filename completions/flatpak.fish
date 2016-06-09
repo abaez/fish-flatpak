da8ba1b3835ad69643cd5239a1ce3c1ad72efeb2
Alejandro Baez <code@baez.nyc>
2016-06-09 19:05:32 -0400

    EVERYTHING REFACTORED. IT WORKS

--- completions/flatpak.fish
+++ completions/flatpak.fish
@@ -1,37 +1,11 @@
 
+# the commands
+set __fish_flatpak_subcommands install update uninstall list info run \
+  override export-file enter remote-add remote-modify remote-delete   \
+  remote-list remote-ls build-init build build-finish build-export    \
+  build-bundle build-import-bundle build-sign build-update-repo
 
-function   __fish_flatpak_command
-  set -l cmdline (commandline -poc)
-  set -e cmdline[1]
-  set -l lasttoken ""
-  for token in $cmdline
-    if string match -r -q -- "-v|--verbose|--version"
-      set lasttoken $token
-      continue
-    end
-
-    echo $token
-    return 0
-  end
-
-  # no command found
-  return 1
-end
-
-function __fish_flatpak_subcommand --argument-names cmd
-  if set -l token (__fish_flatpak_command)
-    string match -q -- $token $cmd
-    return 0
-  else
-    return 1
-  end
-end
-
-for cmd in  install update uninstall list info run override export-file enter remote-add remote-modify remote-delete remote-list remote-ls build-init build build-finish build-export build-bundle build-import-bundle build-sign build-update-repo
-
-  complete -c flatpak -n "not __fish_flatpak_subcommand" -xa $cmd
-end
-
+complete -c flatpak -f -c flatpak -n "__fish_use_subcommand" -a "$__fish_flatpak_subcommands"
 
 # flatpak
 complete -c flatpak -s h -l help -d "Show help options and exit."
@@ -65,17 +39,17 @@ function __fish_flatpak_samesub
 end
 
 # install
-complete -c flatpak -n "__fish_flatpak_command install" -l bundle -d "Install from a bundle file instead of a configured remote."
+complete -c flatpak -n "__fish_seen_subcommand_from install" -l bundle -d "Install from a bundle file instead of a configured remote."
 __fish_flatpak_samesub install user system arch subpath no-deploy no-pull app runtime
 
 # update
-complete -c flatpak -n "__fish_flatpak_command update" -l commit -d "Update to this commit, instead of the tip of the branch."
+complete -c flatpak -n "__fish_seen_subcommand_from update" -l commit -d "Update to this commit, instead of the tip of the branch."
 __fish_flatpak_samesub update user system arch subpath no-doploy no-pull app runtime
 
 # uninstall
-complete -c flatpak -n "__fish_flatpak_command uninstall" -l keep-ref -d "Keep the ref for the application and the objects belonging to it in the local repository."
+complete -c flatpak -n "__fish_seen_subcommand_from uninstall" -l keep-ref -d "Keep the ref for the application and the objects belonging to it in the local repository."
 __fish_flatpak_samesub uninstall user system arch app runtime
 
 # list
-complete -c flatpak -n "__fish_flatpak_command uninstall" -l show-details -d "Show arches and branches, in addition to the application names."
+complete -c flatpak -n "__fish_seen_subcommand_from uninstall" -l show-details -d "Show arches and branches, in addition to the application names."
 __fish_flatpak_samesub list user system app runtime
